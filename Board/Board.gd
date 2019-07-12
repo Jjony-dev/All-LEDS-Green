6f167c27be2bda562bad8539c34dcec877885446
Jjony-dev <developerjjm@gmail.com>
2019-07-12 19:01:11 -0300

    Agregada logica del funcionamiento del tablero

--- Board/Board.gd
+++ Board/Board.gd
@@ -5,9 +5,12 @@ export (int,3,6) var board_size : = 3
 export (int) var board_px_size : = 256
 var scale_factor : float = 1.0
 var positions : Array = []
+var random : RandomNumberGenerator
 
 
 func _ready():
+	random = RandomNumberGenerator.new()
+	random.seed = 12345
 	scale_factor = board_px_size / float(board_size)
 	$ColorRect.rect_size = Vector2(board_px_size, board_px_size)
 	init_board()
@@ -21,10 +24,37 @@ func init_board() -> void:
 			piece.scale *= scale_factor / piece.get_size()
 			piece.set_board_pos(Vector2(r, c))
 			if piece.connect("piece_pressed", self, "_Piece_on_Board_pressed"):
-				print_debug("error connetion -> signal: pressed")
+				print_debug("error connetion -> signal: piece_pressed")
 			positions[r].append(piece)
 			add_child(positions[r][c])
+	start_match()
 
+
+func start_match() -> void:
+	var iterations = 100 + random.randi() % 100 * pow(board_size,2)
+	for i in iterations:
+		var random_positon := Vector2(random.randi() % board_size,
+				random.randi() % board_size)
+		positions[random_positon.x][random_positon.y]._on_Piece_pressed()
+
+func switch_neighbors(_piece : Node2D) -> void:
+	var neighbor_pos : Vector2 = _piece.get_board_pos() + Vector2.UP
+	if is_board_position(neighbor_pos):
+		positions[neighbor_pos.x][neighbor_pos.y].switch_LED()
+	neighbor_pos = _piece.get_board_pos() + Vector2.DOWN
+	if is_board_position(neighbor_pos):
+		positions[neighbor_pos.x][neighbor_pos.y].switch_LED()
+	neighbor_pos = _piece.get_board_pos() + Vector2.LEFT
+	if is_board_position(neighbor_pos):
+		positions[neighbor_pos.x][neighbor_pos.y].switch_LED()
+	neighbor_pos = _piece.get_board_pos() + Vector2.RIGHT
+	if is_board_position(neighbor_pos):
+		positions[neighbor_pos.x][neighbor_pos.y].switch_LED()
+
+
+func is_board_position(_position : Vector2) -> bool:
+	return _position.x >= 0 and _position.x < board_size and _position.y >= 0 and _position.y < board_size
 
 func _Piece_on_Board_pressed(_piece : Node2D) -> void:
+	switch_neighbors(_piece)
 	print(_piece.get_board_pos())
--- Piece/Piece.gd
+++ Piece/Piece.gd
@@ -5,7 +5,7 @@ signal piece_pressed(this_node)
 export (Color) var color_default : = Color.white
 export (Color) var color_on : = Color.green
 export (Color) var color_off : = Color.red
-var on : bool = true
+var on : bool = true setget set_on, is_on
 onready var anim_color_time : = $ColorAnim
 
 func _ready():
@@ -13,14 +13,21 @@ func _ready():
 	random_color_anim(3)
 
 
+func set_on(_on : bool) -> void:
+	on = _on
+	if on:
+		modulate = color_on
+	else:
+		modulate = color_off
+
+
+func is_on() -> bool:
+	return on
+
+
 #Switch between on/off
 func switch_LED() -> void:
-	if on:
-		modulate = color_off
-		on = false
-	else:
-		modulate = color_on
-		on = true
+	set_on(!on)
 
 
 #Change color piece randomly (not change var on)
@@ -38,6 +45,7 @@ func random_color_anim(time : float = 5.0) -> void:
 				modulate = color_off
 	if connect("pressed", self, "_on_Piece_pressed"):
 		print_debug("error connetion -> signal: pressed")
+	set_on(is_on())
 
 
 func set_board_pos(board_pos : Vector2) -> void:
@@ -50,7 +58,8 @@ func get_board_pos()-> Vector2:
 
 func get_size() -> float:
 	return normal.get_size().x
-	
+
+
 #Change Piece color when is pressed
 func _on_Piece_pressed():
 	switch_LED()
